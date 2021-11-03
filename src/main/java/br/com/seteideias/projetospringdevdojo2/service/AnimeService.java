b51cccfc185cc833908ed491c2cd4ef7e4fc2284
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2021-11-03 13:26:18 -0300

    atualizacao docker compose

--- src/main/java/br/com/seteideias/projetospringdevdojo2/service/AnimeService.java
+++ src/main/java/br/com/seteideias/projetospringdevdojo2/service/AnimeService.java
@@ -5,12 +5,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
 
-    private List<Anime> animes = List.of(new Anime(1L,"He-man"),new Anime(2L,"She-haa"),new Anime(3L,"ThunderCats"));
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L,"He-man"),new Anime(2L,"She-haa"),new Anime(3L,"ThunderCats")));
+    }
 
     public List<Anime> listAll(){
         return animes;
@@ -25,4 +31,18 @@ public class AnimeService {
                 ;
     }
 
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3,1000000));
+        animes.add(anime);
+        return anime;
+    }
+
+    public void delete(long id) {
+        animes.remove(findById(id));
+    }
+
+    public void replace(Anime anime) {
+        delete(anime.getId());
+        animes.add(anime);
+    }
 }
