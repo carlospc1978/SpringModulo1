0bdfa04d7b4066f6da5c415625e00d573487975a
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2021-11-04 20:48:23 -0300

    alteracoes e funcionando

--- src/main/java/br/com/seteideias/projetospringdevdojo2/controller/AnimeController.java
+++ src/main/java/br/com/seteideias/projetospringdevdojo2/controller/AnimeController.java
@@ -5,6 +5,7 @@ import br.com.seteideias.projetospringdevdojo2.service.AnimeService;
 import br.com.seteideias.projetospringdevdojo2.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,6 +31,23 @@ public class AnimeController {
     @GetMapping(path = "/{id}")
     public ResponseEntity<Anime> findById(@PathVariable long id){
         return ResponseEntity.ok(animeService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime){
+        return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id){
+        animeService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping
+    public ResponseEntity<Anime> replace(@RequestBody Anime anime){
+        animeService.replace(anime);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 
--- src/main/java/br/com/seteideias/projetospringdevdojo2/service/AnimeService.java
+++ src/main/java/br/com/seteideias/projetospringdevdojo2/service/AnimeService.java
@@ -5,16 +5,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
 
-    private List<Anime> animes = List.of(new Anime(1L,"He-man"),new Anime(2L,"She-haa"),new Anime(3L,"ThunderCats"));
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L,"He-man"),new Anime(2L,"She-haa"),new Anime(3L,"ThunderCats")));
+    }
 
     public List<Anime> listAll(){
         return animes;
     }
+
 
     public Anime findById(long id){
         return animes.stream()
@@ -24,4 +31,18 @@ public class AnimeService {
                 ;
     }
 
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3,1000000));
+        animes.add(anime);
+        return anime;
+    }
+
+    public void delete(long id) {
+        animes.remove(findById(id));
+    }
+
+    public void replace(Anime anime) {
+        delete(anime.getId());
+        animes.add(anime);
+    }
 }
